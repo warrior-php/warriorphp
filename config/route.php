a917319d07ff9172cd2895c7217226e952369f28
dev <support@warriorphp.com>
2025-11-06 00:36:51 -0500

    错误处理路由

--- config/route.php
+++ config/route.php
@@ -1,26 +1,14 @@
 <?php
 declare(strict_types=1);
 
-/**
- * This file is part of webman.
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the MIT-LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @author    walkor<walkor@workerman.net>
- * @copyright walkor<walkor@workerman.net>
- * @link      http://www.workerman.net/
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- */
-
 use App\Service\RouteService;
 use Support\Request;
 use Webman\Route;
 
-//// 禁用默认路由
+// 禁用默认路由
 Route::disableDefaultRoute();
-//// 注册应用路由
+
+// 注册应用路由
 RouteService::registerRoutes();
 
 // 错误处理路由
@@ -40,9 +28,17 @@ Route::fallback(function (Request $request, $status) {
 
     $responseData = [
         'code'        => $status,
-        'data'        => $map[$status],
+        'data'        => $map[$status] ?? [],
         'request_url' => $request->uri(),
         'timestamp'   => time()
     ];
-    return $request->expectsJson() ? json($responseData)->withStatus($status) : view('error', $responseData, 'public')->withStatus($status);
+
+    // 分别处理 404 与 405
+    if ($status === 405) {
+        // 405 强制返回 JSON
+        return json($responseData)->withStatus(405);
+    }
+
+    // 404 返回模板
+    return view('error', $responseData, 'public')->withStatus(404);
 });
