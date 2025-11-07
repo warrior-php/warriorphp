3f19b8a7e7fa64678d8f5d798e1396d3c25b3598
dev <support@warriorphp.com>
2025-11-07 05:10:15 -0500

    管理员登录

--- resource/translations/en/messages.php
+++ resource/translations/en/messages.php
@@ -10,15 +10,17 @@ return [
     // 后台相关
     'admin'        => [
         'login' => [
-            'key1' => 'Username',
-            'key2' => 'Password',
-            'key3' => 'or sign in with',
-            'key4' => 'Google',
-            'key5' => 'Facebook',
-            'key6' => 'Remeber this Device',
-            'key7' => 'Sign In',
-            'key8' => 'Username required',
-            'key9' => 'Login password is required',
+            'key1'  => 'Username',
+            'key2'  => 'Password',
+            'key3'  => 'or sign in with',
+            'key4'  => 'Google',
+            'key5'  => 'Facebook',
+            'key6'  => 'Remeber this Device',
+            'key7'  => 'Sign In',
+            'key8'  => 'Username required',
+            'key9'  => 'Login password is required',
+            'key10' => 'Image CAPTCHA',
+            'key11' => 'Image verification code is required.',
         ]
     ],
 ];
