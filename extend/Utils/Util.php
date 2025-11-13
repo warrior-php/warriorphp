c10eb164b3d1bb4e18639fd8f66c4d1c0e2c72e6
dev <support@warriorphp.com>
2025-11-13 17:53:34 -0500

    新增一些函数

--- extend/Utils/Util.php
+++ extend/Utils/Util.php
@@ -2,6 +2,7 @@
 
 namespace extend\Utils;
 
+use InvalidArgumentException;
 use support\exception\BusinessException;
 use Throwable;
 use Workerman\Timer;
@@ -12,12 +13,12 @@ class Util
     /**
      * 密码哈希
      *
-     * @param        $password
+     * @param string $password
      * @param string $algo
      *
      * @return string
      */
-    public static function passwordHash($password, string $algo = PASSWORD_DEFAULT): string
+    public static function passwordHash(string $password, string $algo = PASSWORD_DEFAULT): string
     {
         return password_hash($password, $algo);
     }
@@ -83,6 +84,7 @@ class Util
                 throw new BusinessException('参数不合法');
             }
         });
+
         return $var;
     }
 
@@ -102,6 +104,7 @@ class Util
                 throw new BusinessException('参数不合法');
             }
         });
+
         return $var;
     }
 
@@ -118,6 +121,7 @@ class Util
         if (!is_string($var) || !preg_match('/^[a-zA-Z0-9_\-\/]+$/', $var)) {
             throw new BusinessException('参数不合法');
         }
+
         return $var;
     }
 
@@ -251,4 +255,108 @@ class Util
         }
         return false;
     }
+
+
+    /**
+     * 隐藏字符串中一部分
+     *
+     * @param string $str
+     * @param int    $start
+     * @param int    $length
+     *
+     * @return string
+     */
+    public static function hideStr(string $str, int $start = 3, int $length = 4): string
+    {
+        return substr_replace($str, '****', $start, $length);
+    }
+
+    /**
+     * 两个任意精度的数字计算
+     *
+     * @param float  $n1     计算数字1
+     * @param string $symbol 计算方式
+     * @param float  $n2     计算数字2
+     * @param int    $scale  精度
+     *
+     * @return string|int
+     */
+    public static function calculate(float $n1, string $symbol, float $n2, int $scale = 2): string|int
+    {
+        switch ($symbol) {
+            case "+": // 加法
+                $res = bcadd($n1, $n2, $scale);
+                break;
+            case "-": // 减法
+                $res = bcsub($n1, $n2, $scale);
+                break;
+            case "*": // 乘法
+                $res = bcmul($n1, $n2, $scale);
+                break;
+            case "/": // 除法
+                if (bccomp($n2, '0', $scale) === 0) {
+                    throw new InvalidArgumentException("Division by zero");
+                }
+                $res = bcdiv($n1, $n2, $scale);
+                break;
+            case "%": // 取模
+                $res = bcmod($n1, $n2);
+                break;
+            default: // 比较大小 > = <
+                $res = bccomp($n1, $n2, $scale);
+                break;
+        }
+        return $res;
+    }
+
+    /**
+     * 金额格式化函数
+     *
+     * @param float|string|int|null $value        待格式化金额
+     * @param int                   $decimals     小数位数，默认 2
+     * @param string                $decimalSep   小数点分隔符，默认 "."
+     * @param string                $thousandsSep 千分位分隔符，默认 ","
+     *
+     * @return string
+     */
+    public static function priceFormat(float|string|int|null $value, int $decimals = 2, string $decimalSep = '.', string $thousandsSep = ','): string
+    {
+        if ($value === null || $value === '') {
+            $value = 0;
+        }
+
+        // 强制转换为浮点数
+        $value = (float)$value;
+
+        return number_format($value, $decimals, $decimalSep, $thousandsSep);
+    }
+
+    /**
+     * 获取字符串长度（支持多语言、多字节字符）
+     *
+     * @param string $str      输入字符串
+     * @param int    $multiLen 多字节字符长度，默认 2
+     * @param string $encoding 字符编码，默认 UTF-8
+     *
+     * @return int
+     */
+    public static function getStrLen(string $str, int $multiLen = 2, string $encoding = 'UTF-8'): int
+    {
+        if ($multiLen <= 1) {
+            // 简单返回字符数
+            return mb_strlen($str, $encoding);
+        }
+
+        $length = 0;
+        $chars = preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
+
+        foreach ($chars as $char) {
+            // 单字节字符（ASCII）按 1 计，多字节字符按 $multiLen
+            $length += (strlen($char) === 1) ? 1 : $multiLen;
+        }
+
+        return $length;
+    }
+
+
 }
