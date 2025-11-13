affdca9dabf41c6d51d66da9d0dccab6aa4ccbe7
dev <support@warriorphp.com>
2025-11-13 17:11:09 -0500

    模型格式化日期

--- app/Model/BaseModel.php
+++ app/Model/BaseModel.php
@@ -3,9 +3,20 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use DateTimeInterface;
 use support\Model;
 
 class BaseModel extends Model
 {
-
+    /**
+     * 格式化日期
+     *
+     * @param DateTimeInterface $date
+     *
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
