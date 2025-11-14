2be40c7f625200c6a6567d9c89ba26199d07f2be
dev <support@warriorphp.com>
2025-11-14 00:54:14 -0500

    add process

--- app/Middleware/Api.php
+++ app/Middleware/Api.php
@@ -3,7 +3,21 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Webman\Http\Request;
+use Webman\Http\Response;
+
 class Api extends BaseMiddleware
 {
-
+    /**
+     * 处理请求
+     *
+     * @param Request  $request
+     * @param callable $handler
+     *
+     * @return Response
+     */
+    public function process(Request $request, callable $handler): Response
+    {
+        return $request->method() == 'OPTIONS' ? response('') : $handler($request);
+    }
 }
